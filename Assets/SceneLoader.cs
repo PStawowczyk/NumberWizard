3e25ba37ef3655afb7756eccc235b5b1c478a58a
PStawowczyk <alexis135832@wp.pl>
2019-06-04 15:00:12 +0200

    .
    .

---
+++ Assets/SceneLoader.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void loadGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void loadWin()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
